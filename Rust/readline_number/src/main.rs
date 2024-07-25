3068358190d267950d3b4ab6188564ff7a87dbe6
hinata <69995747+hinata-117@users.noreply.github.com>
2024-07-25 13:38:17 +0900

    Update main.rs
    Function specification change

--- Rust/readline_number/src/main.rs
+++ Rust/readline_number/src/main.rs
@@ -1,36 +1,9 @@
 extern crate numeric;
 
-/**
- * メイン処理
- * 
- * @param なし
- * @return なし
-*/
 fn main() {
-    test_readline_i32();
-    test_readline_f32();
-}
-/**
- * i32数値入力テスト
- * 
- * @param なし
- * @return なし
-*/
-fn test_readline_i32() {
-    let ret = numeric::read_line::<i32>("Please enter any number here: ".to_string(), 0, 3);
-    if ret.result {
-        println!("[OK] {}\n", ret.val);
-    }
-}
-/**
- * f32数値入力テスト
- * 
- * @param なし
- * @return なし
-*/
-fn test_readline_f32() {
-    let ret = numeric::read_line::<f32>("Please enter any number here: ".to_string(), 0.0, 3);
-    if ret.result {
-        println!("[OK] {}\n", ret.val);
-    }
+    let ret = numeric::read_line::<i32>("Please enter any number here: ".to_string(), 3);
+    match ret {
+        Some(value) => println!("[OK] val = {:?}\n", value),
+        None => println!("[ERR] retry over!!\n"),        
+    };
 }
